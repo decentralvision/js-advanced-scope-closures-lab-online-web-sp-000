752ccf264c4ca212eabb964d36f1e072fb002e93
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-07-11 11:09:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function produceDrivingRange(range) {
   return function(bar, baz) {
     const [barInt, bazInt] = [parseInt(bar), parseInt(baz)]
     const distance = Math.abs(barInt - bazInt)
-    if (distance > range)
+    if (distance > range) {
+      return `${range - distance} out of range`
+    } else {
+      return `within range by ${range - distance}`
+    }
   }
 }
